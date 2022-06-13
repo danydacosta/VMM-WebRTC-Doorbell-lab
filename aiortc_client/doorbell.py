ac98447b6de6fa2745957932f83598e0e83d35be
danydacosta <danydc@powerfoxsa.ch>
2022-06-13 14:21:32 +0200

    Update doorbell.py

--- aiortc_client/doorbell.py
+++ aiortc_client/doorbell.py
@@ -105,11 +105,12 @@ async def main():
             break
 
         # Add the SDP from the 'invite' to the peer connection.
+        print(RTCSessionDescription(data['sdp'], data['type']))
         await pc.setRemoteDescription(RTCSessionDescription(data['sdp'], data['type']))
         # Generate the local session description (answer)
         await pc.setLocalDescription(await pc.createAnswer())
         # send it as 'ok' to the signaling server
-        await sio.emit('ok', { 'sdp' : pc.localDescription.sdp, 'type' : pc.setLocalDescription.type })
+        await sio.emit('ok', { 'sdp' : pc.localDescription.sdp, 'type' : pc.localDescription.type })
 
         await sio.disconnect()
 
